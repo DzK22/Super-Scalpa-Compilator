63a7c55a5b9b1b03e91773561dcbc511debe191e
nbelaribi <you@example.com>
2020-12-23 19:19:17 +0100

    tests basic script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ for file in `ls tests`; do
+  echo "Compiling File ----------> $file"
+  ./cs.out tests/$file
+  echo ""
+
+ done
+
+ for file in `ls *.s`; do
+  echo "Run MIPS  -------------------> $file"
+  spim -f $file
+  echo ""
+ done
