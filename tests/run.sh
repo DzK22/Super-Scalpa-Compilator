498c05e6cb1c2b00d4bf7f1071f072b5123c6d40
nbelaribi <you@example.com>
2020-12-31 21:02:19 +0100

    add some tests

--- tests/run.sh
+++ tests/run.sh
@@ -31,21 +31,20 @@ reset=`tput sgr0`
  mv *.s mips/
 
  i=1
- total=12 # total number of tests
+ total=15 # total number of tests
 
  for file in `ls mips`; do
    echo "----------  SPIM -f $file  ----------"
    spim -f mips/$file | tail -n +6 > tmp_res
    if cmp -s tmp_res tests/results/$file.res; then
-        echo  "${green}>>>>>>>>>>> Test $i/$total mips/$file  passed ${reset} "
-        i=$((i+1))
-
+        echo  "${green}>>>>>>>>>>> Test $i/$total mips/$file  PASSED ${reset} "
+        ((i++))
    else
         echo "We are waiting results like :  "
         cat tests/results/$file.res
         echo "We got this  :  "
         cat tmp_res
-        echo  "${red}>>>>>>>>>>> Test mips/$file failed ${reset}"
+        echo  "${red}>>>>>>>>>>> Test mips/$file FAILED ${reset}"
         exit 1 ;
    fi
    echo ""
